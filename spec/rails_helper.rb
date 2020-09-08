83fdb6eedfa889ce8c4c97cfe1913a4945de6bc7
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-08 12:31:08 +0200

    Created first test in user_can_see_list_of_articles_spec.rb, to display first and second article. Test fail

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,7 @@ end
 
 Shoulda::Matchers.configure do |config|
         config.integrate do |with|
-                with.test_framwork :rspec
+                with.test_framework :rspec
                 with.library :rails
   end
 end
