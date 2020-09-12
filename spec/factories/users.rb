9b13930ad611b6b9434f58ddc9fd5d149c3eb823
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-12 12:49:41 +0200

    Modifying user.rb and user_spec.rb, starting rails s, and log in function shows

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    email { "user1@email.com" }
+    password { "password"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB tabel' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :password }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+  end
+
+  describe 'Factory' do
+    it 'should have vaild Factory' do
+      expect(FactoryBot.create(:user)).to be_valid
+    end
+  end
 end
